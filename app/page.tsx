d70e3cc3cbfbc96426917a9a96b3bd28d02b31f5
ThatFunSize <foulofdoom@gmail.com>
2024-08-07 23:58:50 -0600

    fixed very big issue where site turned white when on a light-mode device & fixed sponsors page formatting

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
    // bg-gray-100
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-      <div className="hidden lg:flex flex flex-row flex-wrap pt-24 ">
-        <div className="h-16 mr-24 flex-1 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-left">
+      <div className="hidden lg:flex flex-row flex-wrap pt-24 ">
+        <div className="h-16 mr-24 flex-1 group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:border-neutral-700 hover:bg-neutral-800/30 lg:text-left">
           <h2 className="mb-3 text-xl font-semibold">
             Hive Members
           </h2>
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="basis-1/3">
           <Carousel images={images} />
         </div>
-        <div className="h-16 ml-24 flex-1 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-right">
+        <div className="h-16 ml-24 flex-1 group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:border-neutral-700 hover:bg-neutral-800/30 lg:text-right">
           <a
             className="pointer-events-none   lg:pointer-events-auto "
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -43,12 +43,12 @@ export default function Home() {
       </div>
 
       <div className=" lg:hidden flex flex-row pt-16 py-0 px-0 ">
-        <div className="left-0 top-0 w-32  relative text-center  group rounded-lg border border-transparent  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ">
+        <div className="left-0 top-0 w-32  relative text-center  group rounded-lg border border-transparent  transition-colors  hover:border-neutral-700 hover:bg-neutral-800/30 ">
           <h2 className="mb-3 mt-3 text-xl font-semibold">
             Hive
           </h2>
         </div >
-        <div className="right-0 top-0 w-32  relative text-center  group rounded-lg border border-transparent  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ">
+        <div className="right-0 top-0 w-32  relative text-center  group rounded-lg border border-transparent  transition-colors  hover:border-neutral-700 hover:bg-neutral-800/30 ">
           
             <h2 className=" mb-3 mt-3 text-xl font-semibold">
               Socials
@@ -57,9 +57,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className=" px-8 relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/3 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[130px] after:w-full after:translate-x-1/6 after:bg-gradient-conic after:from-yellow-200 after:via-yellow-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-yellow-700 before:dark:opacity-10 after:dark:from-yellow-900 after:dark:via-[#FFD700] after:dark:opacity-40 before:w-[400px] after:w-[240px] before:h-[400px]">
+      <div className=" px-8 relative z-[-1] flex place-items-center before:absolute  before:-translate-x-1/3 before:rounded-full  before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[130px] after:w-full after:translate-x-1/6 after:bg-gradient-conic  after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-yellow-700 before:opacity-10 after:from-yellow-900 after:via-[#FFD700] after:opacity-40 before:w-[400px]  before:h-[400px]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#FDE9B160]"
+          className="relative drop-shadow-[0_0_0.3rem_#FDE9B160]"
           src="/hive text2.svg"
           alt="Next.js Logo"
           width={500}
@@ -71,7 +71,7 @@ export default function Home() {
       <div className="hidden lg:flex p-24 flex flex-row flex-wrap justify-center lg:mb-0 lg:w-full  lg:text-left">
         <a
           href="https://www.firstinspires.org/robotics/ftc"
-          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -88,7 +88,7 @@ export default function Home() {
 
         <Link
           href="/sponsors"
-          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
@@ -104,7 +104,7 @@ export default function Home() {
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -121,7 +121,7 @@ export default function Home() {
 
         <Link
           href="/accolades"
-          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="shrink group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
@@ -139,7 +139,7 @@ export default function Home() {
       <div className="pb-36 lg:hidden  flex flex-row flex-wrap justify-center">
         <a
           href="https://www.firstinspires.org/robotics/ftc"
-          className="justify-left shrink group rounded-lg border border-transparent pl-1 pr-3.5 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="justify-left shrink group rounded-lg border border-transparent pl-1 pr-3.5 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -156,7 +156,7 @@ export default function Home() {
 
         <Link
           href="/sponsors"
-          className="shrink group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="shrink group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
           <h2 className="mb-1 text-xl font-semibold">
@@ -172,7 +172,7 @@ export default function Home() {
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="shrink group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="shrink group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -189,7 +189,7 @@ export default function Home() {
 
         <Link
           href="/accolades"
-          className="shrink group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="shrink group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
           <h2 className="mb-1 text-xl font-semibold">
--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -3,37 +3,33 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main className="flex  flex-col items-center justify-between ">
-      
-        
-          <Link
-            href="/"
-            className="self-start mt-24 ml-24 rounded-lg border border-transparent  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 "
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="py-4 relative dark:drop-shadow-[0_0_0.3rem_#FDE9B160]"
-              src="/hive-longway.svg"
-              alt="The Hive"
-              width={300}
-              height={24}
-              priority
-            />
-          </Link>
-        
-        
-          <Link
-            href="/sponsors"
-            className="absolute self-end mt-24 mr-24 rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 "
-            rel="noopener noreferrer"
-          >
-            <h2 className="px-12 py-8 text-right  text-2xl  font-semibold">
-              Socials
-            </h2>
-          </Link>
-        
-      
-      <div className="mt-24 hidden lg:flex self-stretch  place-items-start  flex-row">
-        <div className="shrink-0 pl-24 justify-self-start self-start  place-items-center flex flex-col">
+      <Link
+        href="/"
+        className="self-start mt-12 ml-24 rounded-lg border border-transparent  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30 "
+        rel="noopener noreferrer"
+      >
+        <Image
+          className="py-4 relative drop-shadow-[0_0_0.3rem_#FDE9B160]"
+          src="/hive-longway.svg"
+          alt="The Hive"
+          width={300}
+          height={24}
+          priority
+        />
+      </Link>
+
+      <Link
+        href="/sponsors"
+        className="absolute self-end mt-12 mr-24 rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30 "
+        rel="noopener noreferrer"
+      >
+        <h2 className="px-12 py-8 text-right  text-2xl  font-semibold">
+          Socials
+        </h2>
+      </Link>
+
+      <div className="mt-24 px-24 hidden lg:flex self-stretch  place-items-start  flex-row">
+        <div className="shrink-0  justify-self-start self-start  place-items-center flex flex-col">
           <h2 className="text-3xl font-black">Our Sponsors:</h2>
           <div className=" justify-between  gap-y-8 items-center grid grid-flow-row auto-rows-max">
             <div>
@@ -108,53 +104,60 @@ export default function Page() {
             </div>
           </div>
         </div>
-        <div className="pl-24  flex flex-wrap flex-row">
-          <div className="p-10 group rounded-lg border border-transparent transition-colors border-yellow bg-gray-100 dark:border-neutral-100 dark:bg-neutral-800/30 ">
-            <h2 className="text-4xl font-black mb-1">Who:</h2>
-            <p className="text-xl max-w-[30ch]">
-              A high school FIRST® Tech Challenge robotics team made up of
-              students interested in the STEM field and mentors who are
-              professionals volunteering to help these students.
-            </p>
+        <div className="">
+          <div className="pl-24  flex flex-nowrap flex-row">
+            <div className="p-10 group rounded-lg border border-transparent transition-colors border-yellow bg-gray-100 border-neutral-100 bg-neutral-800/30 ">
+              <h2 className="text-4xl font-black mb-1">Who:</h2>
+              <p className="text-xl max-w-[30ch]">
+                A high school FIRST® Tech Challenge robotics team made up of
+                students interested in the STEM field and mentors who are
+                professionals volunteering to help these students.
+              </p>
+            </div>
+            <div className="p-10 ml-8 group rounded-lg border border-transparent transition-colors border-yellow bg-gray-100 border-neutral-100 bg-neutral-800/30 ">
+              <h2 className="text-4xl font-black mb-1">What:</h2>
+              <p className="text-xl max-w-[30ch]">
+                Every year, we design, build, program, and compete robots in a
+                2v2 challenge. Competing with thousands of teams across the
+                world, we also find funding, make connections, and submit for
+                awards.
+              </p>
+              <a
+                href="https://www.firstinspires.org/robotics/ftc"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline hover:text-blue-400"
+              >
+                Learn More
+              </a>
+            </div>
+            <div className="p-10 ml-8 group rounded-lg border border-transparent transition-colors border-yellow bg-gray-100 border-neutral-100 bg-neutral-800/30 ">
+              <h2 className="text-4xl font-black mb-1">Why:</h2>
+              <p className="text-xl max-w-[30ch]">
+                By taking the role of professionals, we improve our engineering
+                skills while improving teamwork skills. We learn life skills
+                while enjoying the thrill of competition.
+              </p>
+            </div>
           </div>
-          <div className="p-10 ml-8 group rounded-lg border border-transparent transition-colors border-yellow bg-gray-100 dark:border-neutral-100 dark:bg-neutral-800/30 ">
-            <h2 className="text-4xl font-black mb-1">What:</h2>
-            <p className="text-xl max-w-[30ch]">
-              Every year, we design, build, program, and compete robots in a 2v2
-              challenge. Competing with thousands of teams across the world, we
-              also find funding, make connections, and submit for awards.
-            </p>
-            <a
-              href="https://www.firstinspires.org/robotics/ftc"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 underline hover:text-blue-400"
-            >
-              Learn More
-            </a>
-          </div>
-          <div className="p-10 ml-8 group rounded-lg border border-transparent transition-colors border-yellow bg-gray-100 dark:border-neutral-100 dark:bg-neutral-800/30 ">
-            <h2 className="text-4xl font-black mb-1">Why:</h2>
-            <p className="text-xl max-w-[30ch]">
-              By taking the role of professionals, we improve our engineering
-              skills while improving teamwork skills. We learn life skills while
-              enjoying the thrill of competition.
-            </p>
-          </div>
-          <div className="z-[-1] -mt-24">
-            <Image
-              src="/winners.jpg"
-              alt="Hive Members celebrating victory"
-              className=""
-              width={1024}
-              height={24}
-              priority
-            />
-            
-          </div>
-          <div className="pt-12 -ml-6">
-            <h2 className="text-4xl font-black mb-1 max-w-[15ch]">This is us after we qualified for the World Championship for the very first time in the teams history.</h2>
-            <h2 className="text-right mt-36 text-4xl font-black mb-1 max-w-[15ch]">Learn more about us below &#8595;</h2>
+          <div className="pl-24 flex flex-row">
+            <div className="z-[-1] ">
+              <Image
+                src="/winners.jpg"
+                alt="Hive Members celebrating victory"
+                className=""
+                width={1024}
+                height={24}
+                priority
+              />
+            </div>
+            <div className="pt-12 -ml-6">
+              <h2 className="text-4xl font-black mb-1 max-w-[15ch]">
+                This is us after we qualified for the World Championship for the
+                very first time in the teams history.
+              </h2>
+              
+            </div>
           </div>
         </div>
       </div>
