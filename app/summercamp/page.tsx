c7932f064fac1657bf41a0ca2d9f8e20fa8aed7c
nesetkab <foulofdoom@gmail.com>
2025-05-20 00:53:57 -0600

    ts is for kristy

---
+++ app/summercamp/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Header from "@/components/header";
+
+export default function SummerCamp() {
+  return (
+    <main>
+      <Header />
+      <h2> i love kristy so much </h2>
+    </main>
+  );
+}
