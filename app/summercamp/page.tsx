b6ec93332ee288fcc4d6259f68bf98802c6cb434
nesetkab <foulofdoom@gmail.com>
2025-05-21 08:30:31 -0600

    chat look at the cool signup button

--- app/summercamp/page.tsx
+++ app/summercamp/page.tsx
@@ -2,24 +2,45 @@
 
 import Header from "@/components/header";
 import Particles from "@/components/ui/particles";
-import Footer from "@/components/footer";
-import CTA from "@/components/cta";
-import Logos from "@/components/logos";
-import SummerCamp from "@/components/summercamp"
 import { NeonGradientCard } from "@/components/magicui/neon-gradient-card";
-import { Button } from "@/components/ui/button";
+import { EnhancedButton } from "@/components/ui/enhanced-btn";
+import Link from "next/link";
+import { AuroraText } from "@/components/magicui/aurora-text";
+import { LuConstruction } from "react-icons/lu";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center overflow-x-clip pt-12 md:pt-24">
       <section className="flex flex-col items-center px-4 sm:px-6 lg:px-8">
         <Header />
-        <NeonGradientCard>
-          this page is currently under construction. come back later!
-        </NeonGradientCard>
+        <div>
+          <h2 className="text-2xl font-medium mb-20">
+            <LuConstruction className="inline-block text-5xl text-white mr-2" />
+            under construction!
+          </h2>
+        </div>
+        <Link className="px-2" href="https://secureinstantpayments.com/?Event=5580" target="_blank">
+          <NeonGradientCard className="hover:fill-50">
+            <h2 className="text-center font-medium p-0 -m-1 -z-10 ">
+              Sign Up
+            </h2>
+          </NeonGradientCard>
+        </Link>
+
+
+
+
+
 
 
       </section>
+
+      <AuroraText>
+        <h1 className="font-black text-white ">
+          Summer Camp!!!
+        </h1>
+      </AuroraText>
+
 
       <Particles
         quantityDesktop={350}
